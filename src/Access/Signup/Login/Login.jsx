66de7824b34fe5f7418853acef7d25941dddd65e
Muhammad Hasnain <hasnain0893@gmail.com>
2024-08-22 19:47:41 +0500

    working

--- src/Access/Signup/Login/Login.jsx
+++ src/Access/Signup/Login/Login.jsx
@@ -17,47 +17,50 @@ const Login = () => {
                             <div className="mt-2">
                                 <input
                                     id="email"
-                                    placeholder='example@gmai.com'
+                                    placeholder="example@gmail.com"
                                     name="email"
                                     type="email"
                                     required
                                     autoComplete="email"
-                                    className="block w-full  border-0 py-1.5 px-2 text-indigo-950  placeholder:text-gray-400 focus:ring-2  sm:text-sm sm:leading-6"
+                                    className="block w-full border-0 border-b-2 py-1.5 px-2 text-indigo-950 placeholder:text-gray-400 bg-transparent focus:bg-transparent focus:ring-0 focus:outline-none sm:text-sm sm:leading-6"
                                 />
+
+
                             </div>
                         </div>
 
-                        <div>
-                            <div className="flex items-center justify-between">
-                                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                                    Password
-                                </label>
-                                <div className="text-sm">
-                                    <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                                        Forgot password?
-                                    </a>
-                                </div>
-                            </div>
+                        <div className="flex flex-col gap-8 justify-between">
+
                             <div className="mt-2">
                                 <input
                                     id="password"
                                     name="password"
                                     type="password"
+                                    placeholder='*****'
                                     required
                                     autoComplete="current-password"
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    className="block w-full border-0 border-b-2 py-1.5 px-2 text-indigo-950 placeholder:text-gray-400 bg-transparent focus:bg-transparent focus:ring-0 focus:outline-none sm:text-sm sm:leading-6"
                                 />
+                            </div>
+
+                            <div>
+                                <button
+                                    type="submit"
+                                    className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                >
+                                    LOGIN
+                                </button>
+                            </div>
+                            <div className="flex items-center justify-between">
+                                <div className="flex justify-center items-center w-full  text-sm ">
+                                    <a href="#" className="font-semibold text-center text-indigo-600 hover:text-indigo-500">
+                                        Forgot password?
+                                    </a>
+                                </div>
                             </div>
                         </div>
 
-                        <div>
-                            <button
-                                type="submit"
-                                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                            >
-                                Sign in
-                            </button>
-                        </div>
+
                     </form>
 
                     <p className="mt-10 text-center text-sm text-gray-500">
