ac082b1b91b2ef584572bfbe3fa02f5888fb0fff
Muhammad Hasnain <hasnain0893@gmail.com>
2024-08-24 07:25:54 +0500

    working

--- src/Shell/Router.jsx
+++ src/Shell/Router.jsx
@@ -2,6 +2,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../Access/Login";
 import Signup from "../Access/Signup";
 import Home from "../Access/Home";
+import DefaultLayout from '../Layout/DefaultLayout'
+
+const WithLayout = ({ component }) => {
+    return <DefaultLayout>{component}</DefaultLayout>;
+};
 const Router = () => {
     return (
         <BrowserRouter>
@@ -9,6 +14,7 @@ const Router = () => {
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<Signup />} />
+                <Route path="/dashboard" element={<DefaultLayout />} />
             </Routes>
         </BrowserRouter>
     );
